86fda755fe3a06b1771a1a5e794c25ef9c670198
MrRouss <rosaura53able@gmail.com>
2014-11-24 20:58:06 +0100

    Create cabecera.h
    new

---
+++ cabecera.h
@@ -0,0 +1,26 @@
+/*
+ * cabecera.h
+ *
+
+ * GENEDADOR DE PARTIDAS
+ * ::::::::::::::::::::::::::::::::::::::::::::INICIAR PARTIDA::::::::::::::::::::::::::::::::::::::::::::::::
+
+ *  Created on: 24/11/2014
+ *      Author: Android
+ */
+
+#ifndef CABECERA_H_
+#define CABECERA_H_
+
+void imprimirCabecera();
+
+void salir();
+void imprimirEstadisticas();
+int imprimirMenu();
+
+
+
+void imprimirTablero(char * Tablero);
+void imprimirContador(char  tablero[][]);
+
+#endif /* CABECERA_H_ */
